5af843fd76bf6c3114d6c2a52fd74aa328a1a3fa
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-05-08 22:25:47 +0200

    Produce a reservation message to the rapid

--- reservation/src/main/kotlin/no/fortedigital/restaurant/forte/Producer.kt
+++ reservation/src/main/kotlin/no/fortedigital/restaurant/forte/Producer.kt
@@ -3,12 +3,20 @@
  */
 package no.fortedigital.restaurant.forte
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.todayIn
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.StringSerializer
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 fun main() {
     val properties = mapOf(
@@ -21,12 +29,16 @@ fun main() {
     )
     val producer = KafkaProducer<String, String>(properties)
 
-    val topic = "my-topic" // TODO only dummy topic for now
-    val message = """
-        {
-            "message": "Hello from reservation microservice"
-        }
-    """.trimIndent()
+    val topic = "restaurant-forte-rapid-v1" // TODO only dummy topic for now
+    val randomStartTime = Random.nextInt(0 until 22)
+    val randomGuestCount = Random.nextInt(1 until 12)
+
+    val startTime = Clock.System.todayIn(TimeZone.currentSystemDefault()).atTime(hour = randomStartTime, minute = 0)
+    val endTime = Clock.System.todayIn(TimeZone.currentSystemDefault()).atTime(hour = randomStartTime + 1, minute = 0)
+    val totalGuests = TotalGuests(amount = randomGuestCount)
+
+    val reservation = Reservation(startTime = startTime, endTime = endTime, totalGuests = totalGuests).toDTO()
+    val message = Json.encodeToString(reservation)
     val record = ProducerRecord<String, String>(topic, message)
 
     println("Record: ${record.timestamp()} - ${record.value()} ")
--- reservation/src/main/kotlin/no/fortedigital/restaurant/forte/Reservation.kt
+++ reservation/src/main/kotlin/no/fortedigital/restaurant/forte/Reservation.kt
@@ -3,6 +3,7 @@ package no.fortedigital.restaurant.forte
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
+import kotlinx.serialization.Serializable
 import kotlin.time.Duration.Companion.seconds
 
 class Reservation(
@@ -19,4 +20,11 @@ class Reservation(
     val duration
         get() = (endTime.toInstant(TimeZone.currentSystemDefault()).epochSeconds
                         - startTime.toInstant(TimeZone.currentSystemDefault()).epochSeconds).seconds
+}
+
+@Serializable
+data class ReservationDTO(val startTime: LocalDateTime, val endTime: LocalDateTime, val totalGuests: Int, val initiator: String)
+
+internal fun Reservation.toDTO(): ReservationDTO {
+    return ReservationDTO(startTime = startTime, endTime = endTime, totalGuests = totalGuests.amount, initiator = "guest@example.com")
 }
