5d89dffc2b62191e5820be97730bbc5b3f41b037
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-05-08 15:26:54 +0200

    WIP: kafka producer

--- reservation/build.gradle.kts
+++ reservation/build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 
     // Use the JUnit 5 integration.
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.4.0")
 }
 
 kotlin {
